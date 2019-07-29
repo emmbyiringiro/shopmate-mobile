69c5537a345f72bab4ab72b8caa3e35930419b4b
emmbyiringiro <emmbyiringiro@gmail.com>
2019-07-29 11:57:02 +0200

    Intergrate Stripe payment

--- src/components/order/Payment.js
+++ src/components/order/Payment.js
@@ -12,13 +12,57 @@ import { connect } from "react-redux";
 import { theme } from "../../color-themes";
 import { calculateTaxAmount, calculateOrderAmount } from "../../utils";
 import { getTaxes } from "../../actions/tax";
+// This import applied to expo client environment only
+//it'll be switched to  import stripe from 'tipsi-stripe'  in production
+import { DangerZone } from "expo";
+const { Stripe } = DangerZone;
 
 class Payment extends Component {
-  state = { taxPercentage: 0, taxId: 1, description: "" };
+  state = {
+    taxPercentage: 0,
+    taxId: 1,
+    description: "",
+    stripeToken: null,
+    loadingToken: false,
+    errorToken: null
+  };
+
+  componentWillMount() {
+    Stripe.setOptionsAsync({
+      publishableKey: "pk_test_3HWohRBC7VJxG1MTXCOZu8GS00VcOPYR3i",
+      androidPayMode: "test"
+    });
+  }
   componentDidMount() {
     // Get taxes rates  from backend server
     this.props.getTaxes();
   }
+
+  handleCardPayment = async () => {
+    try {
+      this.setState({ loadingToken: true, stripeToken: null });
+      const stripeToken = await Stripe.paymentRequestWithCardFormAsync();
+
+      this.setState({ stripeToken });
+      console.log(stripeToken);
+    } catch (error) {
+      console.log(error);
+      this.setState({ errorToken: error });
+    }
+  };
+  _renderOrderDescription = () => {
+    return (
+      <Input
+        label="Order description"
+        labelStyle={{ fontSize: 15, color: theme.black }}
+        placeholder="Ship to ...."
+        inputStyle={{ fontSize: 14 }}
+        multiline={true}
+        numberOfLines={3}
+        onChangeText={text => this.setState({ description: text })}
+      />
+    );
+  };
 
   _renderOrderTotals = () => {
     const { cart } = this.props;
@@ -82,39 +126,17 @@ class Payment extends Component {
     return (
       <View style={styles.containerStyle}>
         <View style={styles.headerStyle}>
-          <Text style={styles.headerTextStyle}> Tax and Order Total</Text>
+          <Text style={styles.headerTextStyle}> Order Information </Text>
         </View>
-        <View
-          style={{
-            justifyContent: "space-around",
-            alignItems: "center",
-            flexDirection: "row",
-            paddingTop: 10
-          }}
-        >
+        <View style={styles.orderTotalContainer}>
           {this._renderTaxesOptions()}
           {this._renderOrderTotals()}
         </View>
+        {this._renderOrderDescription()}
 
-        <Input
-          label="Order description"
-          labelStyle={{ fontSize: 15, color: theme.black }}
-          placeholder="Ship to ...."
-          inputStyle={{ fontSize: 14 }}
-          multiline={true}
-          numberOfLines={3}
-          onChangeText={text => this.setState({ description: text })}
-        />
-        <View
-          style={{
-            justifyContent: "flex-end",
-            alignItems: "center",
-            padding: 20,
-            backgroundColor: theme.white
-          }}
-        >
+        <View style={styles.placeOrderButtonContainer}>
           <Button
-            onPress={() => console.log("Order Placed ")}
+            onPress={() => this.handleCardPayment()}
             type="solid"
             title="Place Order"
             titleStyle={{ fontSize: 15 }}
@@ -146,7 +168,19 @@ const styles = StyleSheet.create({
   },
 
   headerTextStyle: { fontWeight: "700", fontSize: 16 },
-  totalTextStyle: { fontWeight: "700" }
+  totalTextStyle: { fontWeight: "700" },
+  placeOrderButtonContainer: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: theme.white
+  },
+  orderTotalContainer: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingTop: 10
+  }
 });
 
 export default connect(
